393630363aace541d4d54b97540b90c84f977780
Ardillán <ardillan@me.com>
2020-08-02 15:19:31 +0200

    Add text description to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,19 @@ const IndexPage = () => {
       <ImageContainer>
         <Image />
       </ImageContainer>
+      <h2>We want to show you real tech spaces</h2>
+      <p>
+        It's nice to have your own private space. It's a fact that people are
+        living an important amount of time working and having fun on a desk with
+        computers, videogames, drawing, painting, reading, or even watching
+        movies.
+      </p>
+      <p>
+        In order to be confortable, it's necesary to have everything right in
+        it's place, and that doesn't mean to have our desk clean and ready to
+        "take the perfect shot". At the end, we use our tables as what the was
+        meant to be: tables.
+      </p>
     </Layout>
   );
 };
